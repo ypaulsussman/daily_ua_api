7d534a2ec5f21ca31b1dfeaedbd01d4cde22bc41
Y Paul Sussman <ypaulsussman@gmail.com>
2018-05-08 19:03:10 -0500

    adds scraping to Headline model

--- app/models/headline.rb
+++ app/models/headline.rb
@@ -1,2 +1,18 @@
+require 'open-uri'
+
 class Headline < ApplicationRecord
+
+  def self.scrape
+    page = Nokogiri::HTML(open("https://www.bbc.com/ukrainian"))   
+    new_headlines=[]
+    page.css('div.buzzard-item a.title-link span').each {|x| new_headlines.push(x.text) }
+    page.css('div.dove-item__body a h3 span').each {|x| new_headlines.push(x.text) }
+    page.css('div.macaw-item__body a h3 span').each {|x| new_headlines.push(x.text) }
+
+    new_headlines.each_index do |index|
+      headline = Headline.create(text_ua: new_headlines[index])
+    end
+
+  end
+
 end
