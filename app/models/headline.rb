0a23258184695d4e9fb13350a9bc530d02c107ea
Y Paul Sussman <ypaulsussman@gmail.com>
2018-05-08 21:23:19 -0500

    connects to GCP translation API

--- app/models/headline.rb
+++ app/models/headline.rb
@@ -1,16 +1,28 @@
 require 'open-uri'
+require "google/cloud/translate"
 
 class Headline < ApplicationRecord
 
   def self.scrape
-    page = Nokogiri::HTML(open("https://www.bbc.com/ukrainian"))   
+    project_id = ENV["GCP_PROJECT_ID"]
     new_headlines=[]
+    headlines_en=[]
+    
+    page = Nokogiri::HTML(open("https://www.bbc.com/ukrainian"))   
     page.css('div.buzzard-item a.title-link span').each {|x| new_headlines.push(x.text) }
     page.css('div.dove-item__body a h3 span').each {|x| new_headlines.push(x.text) }
     page.css('div.macaw-item__body a h3 span').each {|x| new_headlines.push(x.text) }
 
+    text = new_headlines
+    translate   = Google::Cloud::Translate.new project: project_id
+    translation = translate.translate text, to: 'en'
+
+    translation.each_index do |index|
+      headlines_en.push(translation[index].text.inspect.gsub!(/\"/, ''))
+    end
+
     new_headlines.each_index do |index|
-      headline = Headline.create(text_ua: new_headlines[index])
+      headline = Headline.create(text_ua: new_headlines[index], text_en_google: headlines_en[index])
     end
 
   end
